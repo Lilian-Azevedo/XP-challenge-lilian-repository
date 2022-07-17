40eb267d88b922341f49be0cc1bdfdb5abdda727
Lilian Azevedo <lilian.azevedo0905@gmail.com>
2022-07-17 00:26:07 -0300

    :lipstick: feat: add some root config css

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,10 +5,10 @@ const Card = (infoStock) => {
   const  { name, qtd, value } = infoStock;
 
   return (
-    <div class="portfolio_data">
-        <h3 class="portfolio_title">{ name }</h3>
-        <p class="portfolio_description">{ qtd }</p>
-        <p class="portfolio_description">{ value }</p>
+    <div class="card_data">
+        <h3 class="card_title">{ name }</h3>
+        <p class="card_description">{ qtd }</p>
+        <p class="card_description">{ value }</p>
 
         <button class="button button--flex">
             C/V

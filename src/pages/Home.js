67316aea59a79a77c7389783bb10bb730e73d43f
Lilian Azevedo <lilian.azevedo0905@gmail.com>
2022-07-20 20:09:50 -0300

    :lipstick: feat: add css title, button, pic - home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import walletPicture from '../images/wallet.png';
+import '../styles/home.css';
+import '../styles/header.css';
 
 const Home = () => {
   const history = useHistory();
@@ -7,14 +10,20 @@ const Home = () => {
   return (
     <div className="details-page">
       <div>
-        <h1>XP Actioners</h1>
+        <h1 className='main-title'>XP Actioners</h1>
       </div>
       <div>
-        <h1>Desenho</h1>
+        <input
+          className='filter-svg big-picture'
+          name="wallet"
+          type="image"
+          src={ walletPicture }
+          alt="home Page"
+        />
       </div>
       <div>
-        <button onClick={ () => history.push('/login') }>Faça Login</button>
-        <button onClick={ () => history.push('/create') }>Crie uma conta</button>
+        <button className='button-general' onClick={ () => history.push('/login') }>Faça Login</button>
+        <button className='button-general' onClick={ () => history.push('/create') }>Crie uma conta</button>
       </div>
     </div>
   );

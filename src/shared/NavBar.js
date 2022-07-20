7659e95ab47b7988c34240cb9979726d7c5025fc
Lilian Azevedo <lilian.azevedo0905@gmail.com>
2022-07-20 19:45:47 -0300

    :construction: feat: add drawn to initial page

--- src/shared/NavBar.js
+++ src/shared/NavBar.js
@@ -7,44 +7,44 @@ const NavBar = () => {
     const [showed, setShowed] = useState(false);
 
     return (
-      <nav class="nav container">
+      <nav className="nav container">
         { showed
-        ?(<div class="nav_menu" id="nav-menu">
-          <ul class="nav_list grid">
+        ?(<div className="nav_menu" id="nav-menu">
+          <ul className="nav_list grid">
 
-            <li class="nav_item">
+            <li className="nav_item">
               <Link to="/user">
                 <Unicons.UilUser size="30" color="#E4B600"/>
               </Link>
               Usuário
             </li>
 
-            <li class="nav_item">
+            <li className="nav_item">
               <Link to="/acount">
                 <Unicons.UilMoneyWithdraw size="30" color="#E4B600"/>
               </Link>
               Conta
             </li>
 
-            <li class="nav_item">
+            <li className="nav_item">
               <Link to="/wallet">
                 <Unicons.UilWallet size="30" color="#E4B600"/>
               </Link>
               Carteira de ações
             </li>
 
-            <li class="nav_item">
+            <li className="nav_item">
               <Link to="/">
                 <Unicons.UilSignOutAlt size="30" color="#E4B600" />
               </Link>
               Sair
             </li>
           </ul>
-          <div onClick={() => setShowed(false)} className='nav_close'>
+          <div onClick={() => setShowed(false)} classNameName='nav_close'>
             <Unicons.UilMultiply  size="30" color="#E4B600" />
           </div>
         </div>)
-        :(<div class="nav_btns" onClick={() => setShowed(true)}>
+        :(<div className="nav_btns" onClick={() => setShowed(true)}>
           <Unicons.UilApps size="30" color="#E4B600" />
         </div>)}
       </nav>

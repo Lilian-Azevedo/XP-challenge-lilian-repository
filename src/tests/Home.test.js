b10b2a3b8672b1eec2193cac74788381b06bad16
Lilian Azevedo <lilian.azevedo0905@gmail.com>
2022-07-21 10:28:19 -0300

    :white_check_mark: tests: started home page tests

--- src/tests/Home.test.js
+++ src/tests/Home.test.js
@@ -2,15 +2,7 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 // import userEvent from '@testing-library/user-event';
 import renderWithRouterAndStore from '../renderWithRouterAndStore';
-// import App from '../App';
-import Home from '../pages/Home';
 import App from '../App';
-
-// beforeEach(() => {
-//     const { history } = renderWithRouterAndStore(<App />);
-//     history.push('/');
-//     // globalHistory = history;
-// });
 
 describe('Teste a página de Home', () => {
   test('Teste se a página contém um title', () => {

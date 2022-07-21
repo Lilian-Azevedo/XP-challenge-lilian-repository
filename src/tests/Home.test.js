42cf6a4e655c1b0585b2361851a37b09560e8a29
Lilian Azevedo <lilian.azevedo0905@gmail.com>
2022-07-21 12:36:15 -0300

    :white_check_mark: tests: verify button login and img

--- src/tests/Home.test.js
+++ src/tests/Home.test.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import renderWithRouterAndStore from '../renderWithRouterAndStore';
 import App from '../App';
 
 describe('Teste a página de Home', () => {
-  test('Teste se a página contém um title', () => {
+  test('1 - Teste se a página contém um title', () => {
     renderWithRouterAndStore(<App />);
     const homeTitle = screen.getByRole('heading', { name: 'XP Actioners', level: 1 });
     expect(homeTitle).toBeTruthy();
+  });
+  test('2 - Teste se a página contém uma imagem em forma de input', () => {
+    renderWithRouterAndStore(<App />);
+    const imgHomePage = screen.getByRole('button', { name: 'home Page' });
+    expect(imgHomePage).toBeTruthy();
+  });
+  test('3 - Teste se a página contém um botão de login que redireciona para a página de login', () => {
+    const { history } = renderWithRouterAndStore(<App />);
+    const buttonLogin = screen.getByRole('button', { name: 'Faça Login' });
+    expect(buttonLogin).toBeTruthy();
+
+    userEvent.click(buttonLogin);
+    expect(history.location.pathname).toBe('/login');
   });
 });

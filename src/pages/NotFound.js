17ccf10ab16dc307e3dd9b034c9936455a76c8b8
Lilian Azevedo <lilian.azevedo0905@gmail.com>
2022-07-23 01:01:01 -0300

    :lipstick: feat: add css to page not found

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,22 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 const NotFound = () => {
+  const history = useHistory();
   return (
     <div className="general-page">
-      <div>
-        <h1>404</h1>
+      <div className='container-not-found'>
+        <h1 className='h1-title'>404</h1>
         <h2>Parece que a página que está procurando não existe!</h2>
         <h3>Se você não faz parte do XP Actioners, crie uma conta!</h3>
       </div>
 
-      <div>
-        <Link to="/">
-            <button type="button">Voltar</button>
-        </Link>
-        <Link to="/create">
-            <button type="button">Cadastrar</button>
-        </Link>
+      <div className='section-btns'>
+        <button
+          className='button-general button--flex'
+          type="button"
+          onClick={() => history.push('/create')}>Cadastrar
+        </button>
+        <button
+          className='button-general button--flex'
+          type="button"
+          onClick={() => history.push('/')}>Página Inicial
+        </button>
       </div>
     </div>);
 }

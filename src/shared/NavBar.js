20f967ff158eeabda2235aa7e3d7f68cadd205f4
Lilian Azevedo <lilian.azevedo0905@gmail.com>
2022-07-24 11:17:32 -0300

    lipstick: feat: add responsive css navbar

--- src/shared/NavBar.js
+++ src/shared/NavBar.js
@@ -4,7 +4,14 @@ import '../styles/navBar.css';
 import { Link } from 'react-router-dom';
 
 const NavBar = () => {
-    const [showed, setShowed] = useState(false);
+    const [showed, setShowed] = useState(window.screen.width < 768 ? false : true);
+
+    const addNavBar = () => {
+      if (window.screen.width < 768) {
+        return setShowed(false);
+      }
+      setShowed(true);
+    }
 
     return (
       <nav className="nav container">
@@ -12,14 +19,14 @@ const NavBar = () => {
         ?(<div className="nav_menu" id="nav-menu">
           <ul className="nav_list grid">
 
-            <li className="nav_item" onClick={() => setShowed(false)}>
+            <li className="nav_item" onClick={ addNavBar }>
               <Link to="/login">
                 <Unicons.UilUser size="30" color="#E4B600"/>
               </Link>
               Trocar Usuário
             </li>
 
-            <li className="nav_item" onClick={() => setShowed(false)}>
+            <li className="nav_item" onClick={ addNavBar }>
               <Link to="/account">
                 <Unicons.UilMoneyWithdraw size="30" color="#E4B600"/>
               </Link>
@@ -27,28 +34,28 @@ const NavBar = () => {
               Retirada
             </li>
 
-            <li className="nav_item" onClick={() => setShowed(false)}>
+            <li className="nav_item" onClick={ addNavBar }>
               <Link to="/wallet">
                 <Unicons.UilWallet size="30" color="#E4B600"/>
               </Link>
               Carteira de Ações
             </li>
 
-            <li className="nav_item" onClick={() => setShowed(false)}>
+            <li className="nav_item" onClick={ addNavBar }>
               <Link to="/">
                 <Unicons.UilSignOutAlt size="30" color="#E4B600" />
               </Link>
               Sair
             </li>
 
-            <li className="nav_item" onClick={() => setShowed(false)}>
+            <li className="nav_item" onClick={ addNavBar }>
               <Link to="/historic">
                 <Unicons.UilHistory size="30" color="#E4B600" />
               </Link>
               Extrato
             </li>
 
-            <li onClick={() => setShowed(false)} className="nav_item" >
+            <li onClick={ addNavBar } className="nav_item nav_close" >
               <Unicons.UilMultiply  size="30" color="#E4B600" />
             </li>
           </ul>
